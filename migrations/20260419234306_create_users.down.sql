9bb1f2683e57abf1057e74611efaf5afbd0e1cad
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-04-21 23:07:44 -0300

    fix migration

--- migrations/20260419234306_create_users.down.sql
+++ migrations/20260419234306_create_users.down.sql
@@ -1,6 +1,1 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(32) NOT NULL,
-    last_name VARCHAR(100),
-    password_hash TEXT NOT NULL
-);
+DROP TABLE users;
--- migrations/20260419234306_create_users.up.sql
+++ migrations/20260419234306_create_users.up.sql
@@ -1,1 +1,6 @@
-DROP TABLE users;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(100),
+    password_hash TEXT NOT NULL
+);
